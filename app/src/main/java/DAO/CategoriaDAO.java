56362a2ab6b8d4f73e6eeed982a5e9de91cdd27b
Matheus Marques <matheusmarquesoliv@gmail.com>
2025-03-19 19:22:44 -0300

    atualização(categoria): atualizei meu model e DAO faltando somente as pesquisas

--- app/src/main/java/DAO/CategoriaDAO.java
+++ app/src/main/java/DAO/CategoriaDAO.java
@@ -13,12 +13,39 @@ public class CategoriaDAO {
     public CategoriaDAO() throws SQLException {
     }
 
-    public void insert(Categoria categoria) throws SQLException {
+    public void insert(Categoria categoria){
         String sql = "INSERT INTO categoria (id_usuario, nome, tipo) VALUES (?, ?, ?)";
-        PreparedStatement stm = conn.prepareStatement(sql);
-        stm.setInt(1, categoria.getId_usuario());
-        stm.setString(2, categoria.getNome());
-        stm.setString(2, categoria.getTipo());
-        stm.executeUpdate();
+        PreparedStatement stm = null;
+        try {
+            stm = conn.prepareStatement(sql);
+            stm.setInt(1, categoria.getId_usuario());
+            stm.setString(2, categoria.getNome());
+            stm.setString(2, categoria.getTipo());
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
+    public void update(Categoria categoria){
+        String sql = "INSERT INTO categoria (id_usuario, nome, tipo) VALUES (?, ?, ?)";
+        try {
+            PreparedStatement stm = conn.prepareStatement(sql);
+            stm.setInt(1, categoria.getId_usuario());
+            stm.setString(1, categoria.getNome());
+            stm.setString(1, categoria.getTipo());
+            stm.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    public void deletar(int id) throws SQLException {
+        String sql = "DELETE FROM categoria WHERE id_categoria = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+    }
+
+
 }
