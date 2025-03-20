e94de39624f77710dfc8ed8827b1f9b34fd042eb
joao.camilo <camilojoaoestevao@gmail.com>
2025-03-20 11:27:04 -0300

    feat: criação do home

---
+++ app/src/main/java/view/home/Home.java
@@ -0,0 +1,36 @@
+package view.home;
+
+import java.util.Scanner;
+
+public class Home extends HomeMetodos {
+    public static void main(String[] args) {
+        Logo();
+        Delay(500);
+
+
+        System.out.println(Assistente + " Olá, Eu sou assistente da AgiFinaças"+ Sorrindo);
+        Delay(1000);
+
+
+        System.out.print(Assistente + " Você já tem Conta?:");
+        Scanner sc = new Scanner(System.in);
+        String resposta = sc.nextLine().toUpperCase();
+
+        while (!resposta.equals("SIM") && !resposta.equals("NAO")){
+            System.out.println("Por favor, responda sim ou nao");
+            resposta = sc.nextLine().toUpperCase();
+        }
+
+        if (resposta.equals("Sim")){
+
+        }
+        else {
+
+        }
+
+
+
+
+
+    }
+}
