2ef32721b602de62405bfb467cc4f6b713b818ae
Caroline Vendramini <caroline.vendramini07@gmail.com>
2025-03-20 21:00:57 -0300

    adicionando DAO do LancamentoCartaoDAO, modificando lancamentoCartao model e cartao model

--- app/src/main/java/model/Cartao.java
+++ app/src/main/java/model/Cartao.java
@@ -6,41 +6,17 @@ import java.util.Date;
 public class Cartao {
     private String nome;
     private int limite;
-    private String dataFechamento;
-    private String dataValidade;
+    private int dataFechamento;
+    private int dataValidade;
     private int idCartao;
     private int idUsuario;
 
-    public Cartao(String nome, int limite, String dataFechamento, String dataValidade, int idUsuario){
+    Cartao(String nome, int limite, int dataFechamento, int dataValidade, int idCartao, int idUsuario){
         this.nome = nome;
         this.limite = limite;
         this.dataFechamento = dataFechamento;
         this.dataValidade = dataValidade;
         this.idCartao = idCartao;
-        this.idUsuario = idUsuario;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setLimite(int limite) {
-        this.limite = limite;
-    }
-
-    public void setDataFechamento(String dataFechamento) {
-        this.dataFechamento = dataFechamento;
-    }
-
-    public void setDataValidade(String dataValidade) {
-        this.dataValidade = dataValidade;
-    }
-
-    public void setIdCartao(int idCartao) {
-        this.idCartao = idCartao;
-    }
-
-    public void setIdUsuario(int idUsuario) {
         this.idUsuario = idUsuario;
     }
 
@@ -52,11 +28,35 @@ public class Cartao {
         return limite;
     }
 
-    public String getDataFechamento(){
+    public int getDataFechamento(){
         return dataFechamento;
     }
 
-    public String getDataValidade(){
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setLimite(int limite) {
+        this.limite = limite;
+    }
+
+    public void setDataFechamento(int dataFechamento) {
+        this.dataFechamento = dataFechamento;
+    }
+
+    public void setDataValidade(int dataValidade) {
+        this.dataValidade = dataValidade;
+    }
+
+    public void setIdCartao(int idCartao) {
+        this.idCartao = idCartao;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public int getDataValidade(){
         return dataValidade;
     }
 
