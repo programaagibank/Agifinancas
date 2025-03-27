22dafb6af20ca83b65814c21a9beee4dc0d9f864
Matheus Marques <matheusmarquesoliv@gmail.com>
2025-03-26 21:01:31 -0300

    feat: criado orçamento

--- app/src/main/java/DAO/CategoriaDAO.java
+++ app/src/main/java/DAO/CategoriaDAO.java
@@ -3,6 +3,7 @@ package DAO;
 import model.Categoria;
 import model.JDBC_Connection;
 import model.Usuario;
+import org.checkerframework.checker.units.qual.A;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -62,7 +63,9 @@ public class CategoriaDAO {
                 return new Categoria(
                         rs.getInt("id_usuario"),
                         rs.getString("nome"),
-                        rs.getString("tipo")
+                        rs.getString("tipo"),
+                        rs.getDouble("limite")
+
                 );
             }
         } catch (SQLException e) {
@@ -81,7 +84,8 @@ public class CategoriaDAO {
                 Categoria categoria = new Categoria(
                         rs.getInt("id_usuario"),
                         rs.getString("nome"),
-                        rs.getString("tipo")
+                        rs.getString("tipo"),
+                        rs.getDouble("limite")
                 );
                 categoria.setId_categoria(rs.getInt("id_categoria"));
                 categorias.add(categoria);
@@ -104,6 +108,62 @@ public class CategoriaDAO {
         }
         return null;
     }
+    public void criarLimite(int idCat, double limite){
+        String sql = "UPDATE categoria SET limite = ? WHERE id_categoria = ?";
+        try (PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setDouble(1, limite);
+            stmt.setInt(2, idCat);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao criar categoria: " + e.getMessage(), e);
+        }
+    }
 
+    public Categoria validacao(int id_categoria, int idUser) {
+        String sql = "SELECT * FROM categoria WHERE id_categoria = ? AND id_usuario = ?";
+        try (PreparedStatement stm = conn.prepareStatement(sql)) {
+            stm.setInt(1, id_categoria);
+            stm.setInt(2, idUser);
+            ResultSet rs = stm.executeQuery();
+            if (rs.next()) {
+                return new Categoria(
+                        rs.getInt("id_usuario"),
+                        rs.getString("nome"),
+                        rs.getString("tipo"),
+                        rs.getDouble("limite")
 
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar categoria: " + e.getMessage(), e);
+        }
+        return null;
+    }
+    public List<Categoria> listaLimites(Usuario user){
+        String sql = "SELECT * FROM categoria WHERE limite > 0 AND id_usuario = ?";
+        List<Categoria> cat = new ArrayList<>();
+        try {
+            PreparedStatement stm = conn.prepareStatement(sql);
+            stm.setInt(1, user.getIdUsuario());
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                Categoria categoria = new Categoria(
+                        rs.getInt("id_usuario"),
+                        rs.getString("nome"),
+                        rs.getString("tipo"),
+                        rs.getDouble("limite")
+                );
+                categoria.setId_categoria(rs.getInt("id_categoria"));
+                cat.add(categoria);
+            }
+            return cat;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
+
+
+
---
+++ app/src/main/java/view/orcamento/OrcamentoView.java
@@ -0,0 +1,56 @@
+package view.orcamento;
+
+import control.OrcamentoControl;
+import model.Categoria;
+import model.Usuario;
+
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAdjusters;
+import java.util.List;
+import java.util.Scanner;
+
+public class OrcamentoView {
+    private static Scanner sc = new Scanner(System.in);
+    public static void startOrcamento(Usuario user) throws SQLException {
+        int op = OrcamentoMetodo.menuOpcao();
+
+        int idCategoria;
+        switch (op){
+            case 1:
+                idCategoria = OrcamentoMetodo.obterIdcat(user);
+                OrcamentoMetodo.cadastrarOrcamento(idCategoria);
+                break;
+            case 2:
+                String dataInicial, dataFinal, ano, mes;
+                System.out.println("Digite o ano que deseja consultar:");
+                ano = sc.nextLine();
+                System.out.println("Digite o mes que deseja consultar: ");
+                mes = sc.nextLine();
+                dataInicial = ano+"-"+mes+"-01";
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                LocalDate data = LocalDate.parse(dataInicial, formatter);
+
+                // Último dia do mês dessa data
+                LocalDate ultimoDiaDoMes = data.with(TemporalAdjusters.lastDayOfMonth());
+
+                // Converter o último dia do mês de volta para string no mesmo formato
+                dataFinal = ultimoDiaDoMes.format(formatter);
+
+                List<Categoria> listOrcamento = OrcamentoControl.listLimite(user);
+                for (Categoria categoria : listOrcamento){
+                    double valorAtual = OrcamentoControl.valorAtualCategoria(categoria, user, dataInicial,dataFinal);
+                    System.out.printf("Categoria: %s --- Limite: R$ %.2f --- Atual: %.2f\n", categoria.getNome(), categoria.getLimite(), valorAtual);
+                }
+                break;
+        }
+
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+        Usuario user = new Usuario(1, "joao@gmail", "1234");
+        OrcamentoView.startOrcamento(user);
+    }
+}
